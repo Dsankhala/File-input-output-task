20c18e3a76412733d76e0d5e89e3e1248d9a0e33
Dimple <dimplesankhla9@gmail.com>
2022-12-21 00:52:52 +0530

    Ragistration

---
+++ Entry.rb
@@ -0,0 +1,18 @@
+Name - rohit Email - rohit@gmail.com Password - rohit123 Confirm password - rohit123 
+Name - tripti Email - tripti@gmail.com Password - tripti@123 Confirm password - tripti 
+Name - dimple Email - hanu  Password - 123 Confirm password - 123 
+Name - pawan Email - pawan@gmail.com Password - pawan456 Confirm password - 456 
+Name - rohit Email - gmail.com Password - google Confirm password - google 
+Name - dddd Email - fffff Password - gggg Confirm password - jjjj 
+Name -  Email -  Password -  Confirm password -  
+Name -  Email -  Password -  Confirm password -  
+Name - plantix Email - plantix@gmail.com Password - 123 Confirm password - 132 
+Name -  Email -  Password -  Confirm password -  
+Name - fghj Email - jhgf Password - 123 Confirm password - 1123 
+Name -  Email -  Password -  Confirm password -  
+Name -  Email -  Password -  Confirm password -  
+Name - dfghjk Email - jhgfd Password - 123 Confirm password - 1233 
+Name - dfghj Email - dfghjk Password - 132 Confirm password - 123 
+Name - fghj Email - mnhg Password - 132 Confirm password - 123 
+Name - fghj Email - kjh Password - 412. Confirm password - 412 
+Name - fghjk Email - fghjkl Password - 123 Confirm password - 123 
